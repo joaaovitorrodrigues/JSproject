acbcc829ea4aaacc5c1ce600cd512da439e5a7c2
jvrody <joaovitorrodrigues@outlook.com.br>
2021-04-30 14:32:39 -0300

    teste

---
+++ aula.js
@@ -0,0 +1,12 @@
+let nome = 'João Vitor';
+let sobrenome = 'Rodrigues';
+let cpf = 43273044844;
+let rg = 457434712;
+
+let pessoa = {
+    nome: 'João Vitor',
+    sobrenome: 'Rodrigues',
+    cpf: 43273044844
+};
+
+console.log(pessoa);
